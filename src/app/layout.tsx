8bf3bda99fdad523ef5b423b4908eaa4451b3595
m01171 <kristian.avlesbug@skatteetaten.no>
2025-03-27 10:41:54 +0100

    pwa status bar color test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html>
         <head>
             <meta name="apple-mobile-web-app-capable" content="yes"/>
-            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+            <meta name="apple-mobile-web-app-status-bar-style" content="red"/>
         </head>
         <body>
             <AuthProvider>{children}</AuthProvider>
